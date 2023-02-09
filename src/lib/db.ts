0ed9f642b4d26fa4fa8712d9736b00b9f5b556b0
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-02-09 09:45:17 -0800

    update production API urls

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
 import { dev } from '$app/environment';
 
-const url = dev ? env.PUBLIC_SUPABASE_LOCAL_URL : env.SUPABASE_URL;
-const key = dev ? env.PUBLIC_SUPABASE_LOCAL_ANON_KEY : env.SUPABASE_ANON_KEY;
-
+const url = dev ? env.PUBLIC_SUPABASE_LOCAL_URL : env.PUBLIC_SUPABASE_URL;
+const key = dev ? env.PUBLIC_SUPABASE_LOCAL_ANON_KEY : env.PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(url, key);
