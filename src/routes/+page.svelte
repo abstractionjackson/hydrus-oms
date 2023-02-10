0c9fa8bba3152057959e3c43b6145215f9bb6872
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-02-10 14:51:45 -0800

    fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	let title = 'Surgical Outcomes Tracker';
-	let description = 'Outcomes management for surrgeons that tracks patient progress.';
+	let description = 'Outcomes management for surgeons that tracks patient progress.';
 </script>
 
 <main class="flex flex-1 justify-center items-center h-full bg-home bg-cover bg-center">
