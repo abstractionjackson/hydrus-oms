7e133ea8af4f10058549527b1649954f7813817a
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-05-23 15:21:37 -0700

    add description and btn sign up to landing

--- src/lib/components/Landing.svelte
+++ src/lib/components/Landing.svelte
@@ -2,16 +2,20 @@
 	import { goto } from '$app/navigation';
 	import logo from '$assets/images/logo4_no_bg.png';
 	export let brandtext = 'Hydrus';
-	export let tagline = 'Microstent surgery outcomes tracking for physicians';
+	export let description: string =
+		'Track patient outcomes for Hydrus microstent surgery, and visualize IOP and medication data across patients and time.';
 </script>
 
 <div class="container">
 	<header>
 		<img src={logo} alt="logo" />
 		<h1>{brandtext}</h1>
-		<!-- <p>{tagline}</p> -->
+		<p>{description}</p>
 	</header>
-	<button on:click={() => goto('/signin')}>Sign In</button>
+	<div class="btn-container">
+		<button on:click={() => goto('/signup')}>Sign Up</button>
+		<button on:click={() => goto('/signin')} class="outline">Sign In</button>
+	</div>
 </div>
 
 <style>
@@ -26,10 +30,20 @@
 	img {
 		width: 100px;
 	}
+	h1 {
+		font-size: 3em;
+		font-weight: 700;
+		margin-bottom: 1rem;
+	}
 	button {
 		width: fit-content;
 		margin: 0 auto;
-		font-size: 1.25em;
 		font-weight: 700;
+	}
+	.btn-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		width: 100%;
 	}
 </style>
